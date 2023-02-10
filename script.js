293b64cdc590b46aac02b0a02158dc852d4731c0
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-02-10 18:19:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,7 @@
 const img = document.querySelector(".item1");
 const btn = document.querySelector(".arrow");
-let libraryArr = [];
-let queueArr = [];
-let id = "Kiwi";
-let addToLibrary = (id) => {
-  let parsedLibrary = JSON.parse(localStorage.getItem("library"));
-
-  if (libraryArr.includes(id) || id === "") {
-    return;
-  } else if (libraryArr.length <= 0 && parsedLibrary !== null) {
-    parsedLibrary.forEach((element) => {
-      libraryArr.unshift(element);
-    });
-    if (libraryArr.includes(id)) {
-      return;
-    } else {
-      libraryArr.unshift(id);
-    }
-  } else {
-    libraryArr.unshift(id);
-  }
-  localStorage.setItem("library", JSON.stringify(libraryArr));
-  // console.log(parsedLibrary);
-  // console.log(libraryArr);
-};
-
-let addToQueue = (id) => {
-  let parsedQueue = JSON.parse(localStorage.getItem("queue"));
-
-  if (queueArr.includes(id) || id === "") {
-    return;
-  } else if (queueArr.length <= 0 && parsedQueue !== null) {
-    parsedQueue.forEach((element) => {
-      queueArr.unshift(element);
-    });
-    if (queueArr.includes(id)) {
-      return;
-    } else {
-      queueArr.unshift(id);
-    }
-  } else {
-    queueArr.unshift(id);
-  }
-  localStorage.setItem("queue", JSON.stringify(queueArr));
-  // console.log(parsedLibrary);
-  // console.log(libraryArr);
-};
 
 btn.addEventListener("click", () => {
-  addToLibrary(id);
-  addToQueue(id);
   img.classList.toggle("hide");
   btn.classList.toggle("arrowRotate");
 });
