f4caaee4499d8adcdcfb3fa6807af9bfe309cc10
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-02-10 14:02:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const img = document.querySelector(".item1");
 const btn = document.querySelector(".arrow");
+let libraryArr = [];
+let queueArr = [];
+let id = "Banana";
+let addToLibrary = (id) => {
+  let parsedLibrary = JSON.parse(localStorage.getItem("library"));
+
+  if (libraryArr.includes(id)) {
+    return;
+  } else if (libraryArr.length <= 0 && parsedLibrary !== null) {
+    parsedLibrary.forEach((element) => {
+      libraryArr.unshift(element);
+    });
+    if (libraryArr.includes(id)) {
+      return;
+    } else {
+      libraryArr.unshift(id);
+    }
+  } else {
+    libraryArr.unshift(id);
+  }
+  console.log(parsedLibrary);
+  console.log(libraryArr);
+};
+
+let addToQueue = () => {
+  queueArr.unshift(movieId);
+  localStorage.setItem("queue", JSON.stringify(queueArr));
+};
 
 btn.addEventListener("click", () => {
+  addToLibrary(id);
+  localStorage.setItem("library", JSON.stringify(libraryArr));
   img.classList.toggle("hide");
   btn.classList.toggle("arrowRotate");
 });
